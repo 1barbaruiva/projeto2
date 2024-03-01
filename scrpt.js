30e32146e686fb9ab878091d4df06dd507422dec
1barbaruiva <flaviooperacional18@gmail.com>
2024-03-01 20:37:18 -0300

    atualização 2

--- scrpt.js
+++ scrpt.js
@@ -14,3 +14,24 @@ function nextImage(){
 
     document.getElementById("radio"+count).checked = true;
 }
+
+const btnMobile = document.getElementById('btn-mobile');
+
+function toggleMenu(event) {
+  if (event.type === 'touchstart') event.preventDefault();
+  const nav = document.getElementById('nav');
+  nav.classList.toggle('active');
+  const active = nav.classList.contains('active');
+  event.currentTarget.setAttribute('aria-expanded', active);
+  if (active) {
+    event.currentTarget.setAttribute('aria-label', 'Fechar Menu');
+  } else {
+    event.currentTarget.setAttribute('aria-label', 'Abrir Menu');
+  }
+}
+
+btnMobile.addEventListener('click', toggleMenu);
+btnMobile.addEventListener('touchstart', toggleMenu);
+    
+
+
